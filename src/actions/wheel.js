7e514a621f152fc055b633920ed94081a4347757
László Simon <Pho3nixHun@users.noreply.github.com>
2020-12-18 10:54:41 +0100

    make event handlers non-passive

--- src/actions/wheel.js
+++ src/actions/wheel.js
@@ -1,26 +1,26 @@
-export function wheel(node) {
-    let dx = 0, dy = 0
+    export function wheel(node) {
+        let dx = 0, dy = 0
 
-    function handleWheel(e) {
-        if (e.shiftKey) {
-            dx = e.deltaY;
-        } else {
-            dx = e.deltaX;
-            dy = e.deltaY;
+        function handleWheel(e) {
+            if (e.shiftKey) {
+                dx = e.deltaY;
+            } else {
+                dx = e.deltaX;
+                dy = e.deltaY;
+            }
+            if (dx !== 0) {
+                e.preventDefault ? e.preventDefault() : (e.returnValue = false);
+            }
+            node.dispatchEvent(new CustomEvent('wheels', {
+                detail: { dx, dy }
+            }));
         }
-        if (dx !== 0) {
-            e.preventDefault ? e.preventDefault() : (e.returnValue = false);
-        }
-        node.dispatchEvent(new CustomEvent('wheels', {
-            detail: { dx, dy }
-        }));
-    }
 
-    node.addEventListener('wheel', handleWheel);
+        node.addEventListener('wheel', handleWheel, { passive: false });
 
-    return {
-        destroy() {
-            node.removeEventListener('wheel', handleWheel);
-        }
+        return {
+            destroy() {
+                node.removeEventListener('wheel', handleWheel);
+            }
+        };
     };
-};
