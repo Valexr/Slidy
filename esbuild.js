41206df34dc0f4d89c59533a91068c315a6b0b15
Valexr <valexr@gmail.com>
2021-03-15 08:38:57 +0300

    2.5.9

--- esbuild.js
+++ esbuild.js
@@ -4,38 +4,38 @@ const pkg = require('./package.json');
 
 (async () => {
 
-	await esbuild.build({
-		entryPoints: ['src/index.js'],
-		outfile: pkg.main,
-		format: 'cjs',
-		bundle: true,
-		minify: true,
-		sourcemap: false,
-		external: ['svelte', 'svelte/*'],
-		plugins: [sveltePlugin({ compileOptions: { css: true } })]
-	});
+    await esbuild.build({
+        entryPoints: ['src/index.js'],
+        outfile: pkg.main,
+        format: 'cjs',
+        bundle: true,
+        minify: true,
+        sourcemap: false,
+        external: ['svelte', 'svelte/*'],
+        plugins: [sveltePlugin({ compileOptions: { css: true } })]
+    });
 
-	await esbuild.build({
-		entryPoints: ['src/index.js'],
-		outfile: pkg.module,
-		format: "esm",
-		bundle: true,
-		minify: true,
-		sourcemap: false,
-		external: ['svelte', 'svelte/*'],
-		plugins: [sveltePlugin({ compileOptions: { css: true } })],
-	});
+    await esbuild.build({
+        entryPoints: ['src/index.js'],
+        outfile: pkg.module,
+        format: "esm",
+        bundle: true,
+        minify: true,
+        sourcemap: false,
+        external: ['svelte', 'svelte/*'],
+        plugins: [sveltePlugin({ compileOptions: { css: true } })],
+    });
 
-	await esbuild.build({
-		entryPoints: ['src/index.js'],
-		outfile: pkg.browser,
-		platform: 'browser',
-		format: "iife",
-		bundle: true,
-		minify: true,
-		sourcemap: false,
-		globalName: "svelteSLidy",
-		plugins: [sveltePlugin({ compileOptions: { css: true } })],
-	});
+    await esbuild.build({
+        entryPoints: ['src/index.js'],
+        outfile: pkg.browser,
+        platform: 'browser',
+        format: "iife",
+        bundle: true,
+        minify: true,
+        sourcemap: false,
+        globalName: "svelteSLidy",
+        plugins: [sveltePlugin({ compileOptions: { css: true } })],
+    });
 
 })()
