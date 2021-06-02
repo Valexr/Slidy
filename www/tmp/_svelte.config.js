0682dc7b00e69bfde8c4ce5a26471ab3359e2e39
Valexr <valexr@gmail.com>
2021-06-02 08:54:31 +0300

    del tmp

--- www/tmp/_svelte.config.js
+++
@@ -1,6 +0,0 @@
-const sveltePreprocess = require('svelte-preprocess');
-const path = require('path');
-
-module.exports = {
-    preprocess: sveltePreprocess({ includePaths: [path.join(__dirname, 'relative/path')] })
-};
