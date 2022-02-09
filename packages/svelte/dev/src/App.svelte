9ae6d84eddfd66595589e02182cace0e6ec8cfbc
Eric Rovell <ericrovell@gmail.com>
2022-02-09 12:40:16 +0300

    refactor: update demo for new component api, wip

--- packages/svelte/dev/src/App.svelte
+++ packages/svelte/dev/src/App.svelte
@@ -1,111 +1,91 @@
-<svelte:options immutable={true} />
+<script lang="ts" context="module">
+    import { getPhotos, randInt } from './scripts/api';
+    import { name, version } from '../../package.json';
+    import Slidy from '../../src/Slidy.svelte';
+
+    import type { Slide } from "./types";
+</script>
+
+<script lang="ts">
+    let slides: Slide[] = [];
+    let position = 0;
+    let page = randInt(0, 90);
+    let limit = 5;
+    let index = 0;
+    let vertical = false;
+    let clamp = true;
+    let align: "start" | "middle" | "end" = "middle";
+    let duration = 375;
+    let stend = false;
+    let gravity = 1.2;
+    let width = 'auto';
+    let snap = true;
+    let images = true;
+    let loop = false;
+    let gap = 15;
+
+    $: dark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    $: theme = dark ? "dark" : "light";
+
+    const loadPhotos = async (limit: number, page: number) => await getPhotos({ limit, page });
+
+    function switchTheme() {
+        dark = !dark;
+        document.documentElement.setAttribute('scheme', theme);
+    }
+
+    const shuffle = () => (page = randInt(0, 90));
+</script>
 
 <fieldset>
     <legend>
         <h1>Slidy {version.replace(/\.[^.]*$/, '')}<sup>svelteJS</sup></h1>
-        <button on:click={() => (stend = !stend)} class:active={stend}
-            >stend</button
-        >
-        <button on:click={() => (images = !images)} class:active={images}
-            >images</button
-        >
-        <button on:click={changeScheme} class:active={dark}>dark</button>
+        <button on:click={() => (stend = !stend)} class:active={stend}>
+            stend
+        </button>
+        <button on:click={() => (images = !images)} class:active={images}>
+            images
+        </button>
+        <button on:click={switchTheme} class:active={dark}>dark</button>
         <p>
             index: [<b>{index}</b>], position: <b>{Math.trunc(position)}</b>px
         </p>
     </legend>
-    <!-- {#if items.length}
-        <label>
-            <input
-                type="range"
-                min="0"
-                max={items.length - 1}
-                step="1"
-                bind:value={index}
-            />
-        </label>
-    {/if} -->
 </fieldset>
 
 <main>
     {#await loadPhotos(limit, page)}
         loading...
-    {:then items}
+    {:then slides}
         <Slidy
-            slides={items}
             bind:index
-            wrap={{
-                id: null,
-                width: '100%',
-                height: '100%',
-                padding: '0',
-                align,
-                alignmargin: 0,
-            }}
-            slide={{
-                gap,
-                class: '',
-                width,
-                height: '100%',
-                backimg: false,
-                imgsrckey: 'src',
-                objectfit: 'cover',
-                overflow: 'hidden',
-            }}
-            controls={{
-                dots: true,
-                dotsnum: true,
-                dotsarrow: true,
-                dotspure: true,
-                arrows: true,
-                keys: true,
-                drag: true,
-                wheel: true,
-            }}
-            options={{
-                vertical,
-                loop,
-                duration,
-                clamp,
-                snap,
-                gravity,
-            }}
             bind:position
-            let:item
-        >
-            {#if images}
-                <img
-                    src={item.src}
-                    alt={item.ix}
-                    width={item.width}
-                    height={item.height}
-                />
-            {/if}
-        </Slidy>
+            {slides}
+            {clamp}
+            {align}
+            {duration}
+            {gravity}
+            {snap}
+            {loop}
+            {vertical}
+        />
     {/await}
 </main>
 
 <nav id="dots">
-    {#each { length: items.length } as dot, i}
-        <button on:click={() => (index = i)} class:active={i === index}
-            >{i}</button
-        >&nbsp;
+    {#each { length: slides.length } as dot, i}
+        <button on:click={() => (index = i)} class:active={i === index}>
+            {i}
+        </button>&nbsp;
     {/each}
 </nav>
 
 <nav>
     <button on:click={() => index--} disabled={!loop && !index}>←</button>
-    <button
-        on:click={() => index++}
-        disabled={!loop && index === items.length - 1}>→</button
-    >
+    <button on:click={() => index++} disabled={!loop && index === slides.length - 1}>→</button>
     <button on:click={shuffle}><i class="icon icon-refresh" /></button>
-    <button on:click={() => (vertical = !vertical)} class:active={vertical}
-        >vertical</button
-    >
-    <button on:click={() => (clamp = !clamp)} class:active={clamp}>
-        clamp
-    </button>
+    <button on:click={() => (vertical = !vertical)} class:active={vertical}>axisY</button>
+    <button on:click={() => (clamp = !clamp)} class:active={clamp}>clamp</button>
     <button on:click={() => (snap = !snap)} class:active={snap}>snap</button>
     <button on:click={() => (loop = !loop)} class:active={loop}>loop</button>
 </nav>
@@ -174,59 +154,14 @@
     </fieldset>
 </form>
 
-<script lang="ts" context="module">
-    import { getPhotos, randomQ } from './scripts/api';
-    import Slidy from '../../src/Slidy.svelte';
-    import { name, version } from '../../package.json';
-
-    import type { Item } from './scripts/api';
-</script>
-
-<script lang="ts">
-    let items: Item[] = [],
-        position = 0,
-        page = randomQ(0, 90),
-        limit = 13,
-        index = 0,
-        vertical = false,
-        clamp = false,
-        align = 'middle',
-        duration = 375,
-        stend = false,
-        gravity = 1.2,
-        width = 'auto',
-        snap = true,
-        images = true,
-        loop = true,
-        gap = 16;
-
-    $: dark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-
-    async function loadPhotos(limit: number, page: number) {
-        items = await getPhotos(limit, page);
-        return items;
+<style>
+    main {
+        --slidy-height: 450px;
     }
 
-    function changeScheme(scheme) {
-        const html = document.documentElement;
-        html.setAttribute('scheme', !dark ? 'dark' : 'light');
-        dark = !dark;
-    }
-    const shuffle = () => (page = randomQ(0, 90));
-
-    const mqList = window.matchMedia('(prefers-color-scheme: dark)');
-    // $: console.log(mqList, dark);
-
-    // $: disabled = !loop && (!index || index > items.length - 1);
-</script>
-
-<style lang="scss">
-    nav {
-        margin: 1rem 0;
-        &#dots {
-            display: flex;
-            overflow-x: scroll;
-        }
+    #dots {
+        display: flex;
+        overflow-x: scroll;
     }
 
     select {
@@ -237,17 +172,19 @@
         display: flex;
         flex-flow: column;
     }
+
     form {
         display: flex;
         flex-wrap: wrap;
-        & > * {
-            flex: 0 auto;
-            gap: 1rem;
-            padding: 0.5rem;
-            display: flex;
-            align-items: center;
-            justify-content: space-around;
-        }
+    }
+
+    form > * {
+        flex: 0 auto;
+        gap: 1rem;
+        padding: 0.5rem;
+        display: flex;
+        align-items: center;
+        justify-content: space-around;
     }
 
     .icon::after,
@@ -259,6 +196,7 @@
         top: 50%;
         transform: translate(-50%, -50%);
     }
+
     .icon-refresh::after {
         border: 0.2em solid currentColor;
         border-top-color: currentcolor;
@@ -270,6 +208,7 @@
         top: -0.25em;
         width: 0;
     }
+
     .icon-refresh::before {
         border: 0.12rem solid currentColor;
         border-right-color: currentcolor;
@@ -278,6 +217,7 @@
         height: 0.75em;
         width: 0.75em;
     }
+
     .icon {
         box-sizing: border-box;
         display: inline-block;
