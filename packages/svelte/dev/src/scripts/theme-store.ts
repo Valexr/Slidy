a5eee34d4a12ae020ef22bce3c6521a4f5c16992
Eric Rovell <ericrovell@gmail.com>
2022-02-18 01:23:57 +0300

    refactor: move out theme logic into store

---
+++ packages/svelte/dev/src/scripts/theme-store.ts
@@ -0,0 +1,18 @@
+import { writable } from "svelte/store";
+
+function themeStore() {
+	const dark = globalThis.window && window.matchMedia('(prefers-color-scheme: dark)').matches;
+	const { subscribe,  update } = writable<boolean>(dark); 
+
+	return {
+		subscribe,
+		switch: () => update(theme => !theme),
+	};
+}
+
+export const darkTheme = themeStore();
+
+darkTheme.subscribe(value => {
+	if (!globalThis.window) return;
+	document.documentElement.setAttribute('scheme',	value ? 'dark' : 'light');
+});
