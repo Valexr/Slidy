fcdadcd2928824f7ee44260d4e78ac973fe2eed0
Eric Rovell <ericrovell@gmail.com>
2022-02-21 14:12:04 +0300

    feat(demo): simple pagination component

---
+++ packages/svelte/dev/src/components/Pagination.svelte
@@ -0,0 +1,32 @@
+<nav class="pagination">
+	{#each { length: pages} as _, i}
+			<button
+				class="pagination-item"
+				on:click={() => (current = i)}
+				class:active={i === current}>
+					{i}
+			</button>
+	{/each}
+</nav>
+
+<script lang="ts">
+	export let current: number;
+	export let pages: number;
+</script>
+
+<style>
+	.pagination {
+		display: flex;
+		flex-flow: row wrap;
+		gap: var(--space-s);
+	}
+
+	.pagination-item {
+		border: none;
+		background-color: var(--surface-3);
+	}
+
+	.pagination-item.active {
+		background-color: var(--accent);
+	}
+</style>
