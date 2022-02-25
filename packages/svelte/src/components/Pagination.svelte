ad1e0c81a26a4d7c199d430bf70bd1e75187f995
Eric Rovell <ericrovell@gmail.com>
2022-02-25 18:18:01 +0300

    feat(slidy-svelte): separate `Pagination` component

--- packages/svelte/src/components/Pagination.svelte
+++ packages/svelte/src/components/Pagination.svelte
@@ -1,55 +1,124 @@
-<nav class="slidy-pagination" class:vertical>
-	{#each { length: pages } as _, i}
-			<slot name="pagination-item" index={i} active={i === current}>
+<nav class="slidy-pagination" class:vertical aria-label="pagination">
+	{#each indices as item}
+			{@const ellipsis = item < 0}
+			{@const title = item < 0 ? undefined : `Show to item #${item}`}
+			{@const active = current === item}
+			<slot name="pagination-item" index={item} active={item === current}>
 				<button
-					data-index={i}
+					data-index={ellipsis ? undefined : item - 1}
 					class="slidy-pagination-item"
 					class:ordinal
-					on:click={() => (current = i)}
-					class:active={i === current}
-					title="Navigate to item #{i}">
-						{ordinal ? i : ""}
+					class:ellipsis
+					disabled={ellipsis}
+					class:active
+					{title}
+					aria-label={title}
+					aria-current={active ? "true" : undefined}>
+						{ordinal ? setContents(item) : ""}
 				</button>
 			</slot>
 	{/each}
 </nav>
 
+<script lang="ts" context="module">
+	import type { IndexGenerator } from "../types";
+
+	const setContents = (index: number): string | number => (index < 0) ? "…" : index;
+
+	/**
+	 * Generates an array of numbers in given range [ start, start + 1, ... );
+	 */
+	const range = (start: number, end: number) => {
+		const size = end - start + 1;
+		return [ ...Array(size).keys() ].map(i => i + start);
+	};
+
+	/**
+	 * end -> the last valid item index
+	 */
+	const generateIndexes: IndexGenerator = ({ current, start = 0, end, limit, siblings }) => {
+		// Total number of items: first + ... + siblings + current + siblings + ... + last
+		const items = Math.max(5 + siblings * 2, end - start + 1);
+
+		// Case: all items are visible
+		if (items <= limit) {
+			return range(start, end);
+		}
+
+		// Calculate left/right sibling indexes at the edge:
+		const siblingLeftIndex = Math.max(current - siblings, start);
+		const siblingRightIndex = Math.min(current + siblings, end);
+
+		/**
+			Do not show dots just when there is just one page number to be inserted
+			between the extremes of sibling and the limits i.e [ 0; lastIndex ].
+		*/
+		const leftDots = siblingLeftIndex > 2;
+    const rightDots = siblingRightIndex < end - 1;
+
+		// Case: ellipsis only at the end:
+		// 1st, sibling, current, sibling, ..., last
+		if (!leftDots && rightDots) {
+			const leftRange = range(start, 3 + 2 * siblings);
+			return [ ...leftRange, -1, end ];
+		}
+
+		// Case: ellipsis only at the end:
+		// 1st, ..., sibling, current, sibling, last
+		if (leftDots && !rightDots) {
+			const rightItems = 3 + 2 * siblings;
+			const rightRange = range(end - rightItems + 1, end);
+			return [ start, -1, ...rightRange ];
+		}
+
+		// Case: ellipsis at both ends:
+		// 1st, ..., sibling, current, sibling, ..., last
+		if (leftDots && rightDots) {
+			let middleRange = range(siblingLeftIndex, siblingRightIndex);
+			return [ start, -1, ...middleRange, -1, end ];
+		}
+
+		return [];
+	};
+</script>
+
 <script lang="ts">
 	export let current: number;
-	export let pages: number;
+	export let start: number;
+	export let end: number;
 	export let ordinal = false;
 	export let vertical = false;
-	//export let max: number = pages;
+	export let limit = 7;
+	export let siblings = 1;
+
+	// Too many items -> should be ordinal for accessibility and responsiveness
+	$: ordinal = (end - start + 1) > limit && true;
+	$: indices = generateIndexes({ current,	start, end, limit, siblings });
 </script>
 
 <style>
 	.slidy-pagination {
 		grid-area: dots;
+		place-self: center;
 
 		display: flex;
 		flex-flow: row wrap;
 		gap: calc(var(--slidy-nav-item-size, 16px) * 0.75);
 		place-content: center;
-
-		z-index: 1;
-		place-self: center;
-		display: flex;
-		place-content: center;
-		backdrop-filter: blur(5px);
 		width: 100%;
 		height: 100%;
 		padding: var(--slidy-nav-item-size, 16px);
+		z-index: 1;		
   }
 
 	.slidy-pagination-item {
 		aspect-ratio: 1 / 1;
 		border-radius: var(--slidy-nav-item-radius, 50%);
-		background-color: var(--slidy-nav-item-color, white);
+		background-color: var(--slidy-nav-item-color, currentColor);
 		opacity: 0.5;
 		width: var(--slidy-nav-item-size, 16px);
 		height: var(--slidy-nav-item-size, 16px);
-		transition: background-color
-				var(--slidy-duration, var(--slidy-duration-default));
+		transition: background-color var(--slidy-duration, 450ms);
 	}
 
 	.slidy-pagination-item.active {
@@ -57,7 +126,7 @@
 	}
 
 	.slidy-pagination-item:focus {
-		outline: 1.5px solid var(--slidy-nav-item-color, white);
+		outline: 1.5px solid var(--slidy-nav-item-color, currentColor);
 		outline-offset: 2px;
 	}
 
@@ -78,6 +147,14 @@
 	.slidy-pagination.vertical {
     flex-flow: column nowrap;
   }
+
+	/* ellipsis */
+	
+	.slidy-pagination-item.ellipsis {
+		background-color: unset;
+		color: inherit;
+		cursor: auto;
+	}
 
 	@media (hover: hover) {
 		.slidy-pagination-item:hover {
--- packages/svelte/src/types.ts
+++ packages/svelte/src/types.ts
@@ -29,3 +29,16 @@ export interface SlidyOptions {
 export type ChangeSlide = (index: number) => void;
 
 export type GetSrc = (item: Slide) => string | undefined;
+
+/**
+ * Pagination indexer parameters.
+ */
+export interface IndexGeneratorParams {
+    current: number;
+    start: number;
+    end: number;
+    limit: number;
+    siblings: number;
+}
+
+export type IndexGenerator = (params: IndexGeneratorParams) => number[];
