976dcc2a1e8e8a09a3fcd032577d6adfaa9bc125
Valexr <valexr@gmail.com>
2022-03-19 20:25:07 +0300

    @slidy/www - add adapter static

--- www/svelte.config.js
+++ www/svelte.config.js
@@ -1,15 +1,26 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
+    // Consult https://github.com/sveltejs/svelte-preprocess
+    // for more information about preprocessors
+    preprocess: preprocess(),
 
-	kit: {
-		adapter: adapter()
-	}
+    kit: {
+        adapter: adapter({
+            // default options are shown
+            pages: 'build',
+            assets: 'build',
+            fallback: null,
+            precompress: false
+        }),
+
+        prerender: {
+            // This can be false if you're using a fallback (i.e. SPA mode)
+            default: true
+        }
+    }
 };
 
 export default config;
