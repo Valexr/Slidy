c10fec130fd2e6f89a604fc9197c67c2f8dee31d
Valexr <valexr@gmail.com>
2022-04-14 11:45:10 +0300

    @slidy/core - mediaStorage

--- packages/core/src/utils/media.ts
+++ packages/core/src/utils/media.ts
@@ -1,39 +1,29 @@
-import { writable } from 'svelte/store';
+export const media = mediaStorage({ key: 'slidy-media' })
 
-export type MediaQuery = {
-    [key: string]: boolean | string;
-};
+function mediaStorage(storage?: Partial<BrowserStorage>, queries?: Partial<MediaQuery>): MediaQuery {
+    const base = {
+        queries: {
+            xs: '(max-width: 480px)',
+            sm: '(max-width: 600px)',
+            md: '(max-width: 840px)',
+            lg: '(max-width: 960px)',
+            xl: '(max-width: 1280px)',
+            xxl: '(min-width: 1281px)',
+            landscape: '(orientation: landscape)',
+            portrait: '(orientation: portrait)',
+            dark: '(prefers-color-scheme: dark)',
+            light: '(prefers-color-scheme: light)',
+            mouse: '(hover: hover)',
+            touch: '(hover: none)'
+        },
+        storage: { type: 'session', key: 'media-query' }
+    }
 
-type BrowserStorage = {
-    type: string
-    key: string
-}
-
-const mediaQueries: MediaQuery = {
-    xs: '(max-width: 480px)',
-    sm: '(max-width: 600px)',
-    md: '(max-width: 840px)',
-    lg: '(max-width: 960px)',
-    xl: '(max-width: 1280px)',
-    xxl: '(min-width: 1281px)',
-    landscape: '(orientation: landscape)',
-    portrait: '(orientation: portrait)',
-    dark: '(prefers-color-scheme: dark)',
-    light: '(prefers-color-scheme: light)',
-    mouse: '(hover: hover)',
-    touch: '(hover: none)',
-};
-
-export const media = watchMedia(mediaQueries, { key: 'gui-media' })
-
-function watchMedia(queries: MediaQuery, storage: Partial<BrowserStorage>) {
-    const { subscribe, set, update } = writable({});
-    const base = { type: 'session', key: 'media-query' }
-
-    storage = { ...base, ...storage }
+    queries = { ...base.queries, ...queries }
+    storage = { ...base.storage, ...storage }
 
     if (persist(storage)) {
-        const match: MediaQuery = JSON.parse(persist(storage).getItem(storage.key as string) as string) || {};
+        const match: MediaQuery = JSON.parse(store(storage).getItem(storage.key as string) as string) || {};
 
         for (const query in queries) {
             const media = window.matchMedia(queries[query] as string)
@@ -41,27 +31,37 @@ function watchMedia(queries: MediaQuery, storage: Partial<BrowserStorage>) {
             media.onchange = (e) => setMatches(e, query)
         }
 
-        subscribe((match: MediaQuery): void => persist(storage).setItem(storage.key as string, JSON.stringify(match)));
-
         function setMatches(media: MediaQueryList | MediaQueryListEvent, query: string) {
             if ('target' in media) match[query] = media.matches;
             else match[query] ??= media.matches;
-            set(match);
-            persist(storage).setItem(storage.key as string, JSON.stringify(match));
+            if (storage)
+                store(storage).setItem(storage.key as string, JSON.stringify(match));
         }
+
+        return match
+    } else return queries
+
+    function store(storage: Partial<BrowserStorage>): Storage {
+        return storage.type === 'session' ? sessionStorage : localStorage
     }
 
-    function persist(storage: Partial<BrowserStorage>): Storage {
+    function persist(storage: Partial<BrowserStorage>): boolean {
         try {
-            const store = storage.type === 'session' ? sessionStorage : localStorage
-            store.setItem(storage.key as string, storage.key as string);
-            store.removeItem(storage.key as string);
-            return store;
+            store(storage).setItem('test' as string, 'test' as string);
+            store(storage).removeItem('test' as string);
+            return true;
         } catch (e) {
             console.error(e)
+            return false
         }
     }
+}
 
-    return { subscribe, set, update }
+type MediaQuery = {
+    [key: string]: boolean | string | undefined;
+};
+type BrowserStorage = {
+    type: string
+    key: string
 }
 
