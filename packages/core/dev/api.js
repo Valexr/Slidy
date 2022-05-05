bb205d5d884cc06998c5d29b38b17333e287e31c
Valexr <valexr@gmail.com>
2022-05-05 19:39:59 +0300

    @slidy/core - dev api

--- packages/core/dev/api.js
+++ packages/core/dev/api.js
@@ -11,11 +11,11 @@ export async function getPhotos(node, page, limit) {
                 node.innerHTML = thumbs.innerHTML = dots.innerHTML = '';
 
                 photos.forEach((p, i) => {
-                    const aspect = aspectQ(p.width, p.height, main.offsetWidth, main.offsetHeight);
+                    const aspect = (value, ratio = false) => aspectQ(p.width, p.height, main.offsetWidth, main.offsetHeight)[value] * (ratio ? devicePixelRatio : 1);
 
-                    node.innerHTML += `<li id="${i}"><img src="https://picsum.photos/id/${p.id}/${aspect.width * window.devicePixelRatio}/${aspect.height * window.devicePixelRatio}.jpg" width="${aspect.width}" height="${aspect.height}" alt="${p.author}"/></li>`;
+                    node.innerHTML += `<li id="${i}"><img src="https://picsum.photos/id/${p.id}/${aspect('width', true)}/${aspect('height', true)}.jpg" width="${aspect('width')}" height="${aspect('height')}" alt="${p.author}"/></li>`;
 
-                    thumbs.innerHTML += `<button id="${i}" style="background-image: url(https://picsum.photos/id/${p.id}/${100 * window.devicePixelRatio}/${100 * window.devicePixelRatio}.jpg)" width="100" height="100" alt="${p.author}">${i}</button>`;
+                    thumbs.innerHTML += `<button id="${i}" style="background-image: url(https://picsum.photos/id/${p.id}/${100 * devicePixelRatio}/${100 * devicePixelRatio}.jpg)" width="100" height="100" alt="${p.author}">${i}</button>`;
 
                     dots.innerHTML += `<button id="${i}">${i}</button>`;
                 });
