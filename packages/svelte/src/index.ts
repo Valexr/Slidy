e1e9a44e76b23bf46c5276cd193d792bad277598
Eric Rovell <ericrovell@gmail.com>
2022-06-29 17:50:53 +0300

    chore(slidy-svelte): update exports

--- packages/svelte/src/index.ts
+++ packages/svelte/src/index.ts
@@ -1,3 +1,5 @@
 export { default as Slidy } from "./components/Slidy/Slidy.svelte";
+export { default as Core } from "./components/Core/Core.svelte";
 export { classNames } from "./components/Slidy/slidy.styles";
 export type { Slide, SlidyOptions } from "./components/Slidy/Slidy.types";
+export type { SlidyCoreOptions } from "./components/Core/Core.types";
