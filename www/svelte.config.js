fd34b21a1c263da9212e8eb2b696bae5cdaa4b59
Eric Rovell <ericrovell@gmail.com>
2022-07-01 01:38:57 +0300

    build(slidy-www): config mdsvex

--- www/svelte.config.js
+++ www/svelte.config.js
@@ -1,26 +1,30 @@
-import adapter from '@sveltejs/adapter-static';
-import preprocess from 'svelte-preprocess';
+import adapter from "@sveltejs/adapter-static";
+import preprocess from "svelte-preprocess";
+import { mdsvex } from "mdsvex";
 
-/** @type {import('@sveltejs/kit').Config} */
+/** @type {import("@sveltejs/kit").Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess(),
-
+	preprocess: [
+		mdsvex(),
+		preprocess()
+	],
+	extensions: [ ".svelte", ".svx" ],
 	kit: {
 		// alias: {
-		//     '@slidy/svelte': path.resolve('../packages/svelte/src/index.ts'),
-		//     '@slidy/media': path.resolve('../packages/media/src/index.ts')
+		//     "@slidy/svelte": path.resolve("../packages/svelte/src/index.ts"),
+		//     "@slidy/media": path.resolve("../packages/media/src/index.ts")
 		// },
 		adapter: adapter({
 			// default options are shown
-			pages: 'build',
-			assets: 'build',
+			pages: "build",
+			assets: "build",
 			fallback: null,
 			precompress: false
 		}),
 		prerender: {
-			// This can be false if you're using a fallback (i.e. SPA mode)
+			// This can be false if you"re using a fallback (i.e. SPA mode)
 			default: false
 		},
 		vite: {
