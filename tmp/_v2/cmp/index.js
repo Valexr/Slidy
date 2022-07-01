b1d852a9cd92449a88906d1d15309eee70b1569d
Valexr <valexr@gmail.com>
2022-07-01 13:39:15 +0300

    updeps & cleaup tmp/v2

--- tmp/_v2/cmp/index.js
+++
@@ -1,3 +0,0 @@
-export { default as Loader } from './Loader.svelte';
-export { default as Arrows } from './Arrows.svelte';
-export { default as Dots } from './Dots.svelte';
--- tmp/_v2/www/src/act/index.js
+++
@@ -1,5 +0,0 @@
-export { clickout } from './clickout.js';
-export { intersection } from './intersection.js';
-export { pannable } from './pannable.js';
-export { resize } from './resize.js';
-export { wheel } from './wheel.js';
