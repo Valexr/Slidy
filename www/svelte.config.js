9081c463929e014f99018e355fb1c4a7748cbf6c
Eric Rovell <ericrovell@gmail.com>
2022-07-06 15:06:25 +0300

    chore(slidy-docs): update deps, manage breaks

--- www/svelte.config.js
+++ www/svelte.config.js
@@ -1,17 +1,6 @@
 import adapter from "@sveltejs/adapter-static";
 import preprocess from "svelte-preprocess";
 import { mdsvex } from "mdsvex";
-import path from "path";
-
-const aliases = [
-	{ name: "@components", path: "./src/lib/components" },
-	{ name: "@lib", path: "./src/lib" },
-	{ name: "@paths", path: "./src/core/paths.ts" },
-	{ name: "@styles", path: "./src/lib/styles" },
-	{ name: "@stores", path: "./src/lib/stores" },
-	{ name: "@utils", path: "./src/lib/utils" },
-	{ name: "@types", path: "./src/types" }
-];
 
 /** @type {import("@sveltejs/kit").Config} */
 const config = {
@@ -28,17 +17,7 @@ const config = {
 			precompress: false
 		}),
 		prerender: {
-			default: false
-		},
-		vite: {
-			resolve: {
-				alias: Object.fromEntries(aliases.map(alias => (
-					[ alias.name, path.resolve(alias.path) ]
-				)))
-			},
-			server: {
-				port: 3339
-			}
+			default: true
 		}
 	}
 };
