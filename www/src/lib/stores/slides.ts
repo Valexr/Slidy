30682ef9f435dbcb1f44d226006651c8eb6977c1
Eric Rovell <ericrovell@gmail.com>
2022-07-11 15:59:53 +0300

    feat(slidy-docs): slides common store

--- www/src/lib/stores/slides.ts
+++ www/src/lib/stores/slides.ts
@@ -1,4 +1,4 @@
-import { derived, writable } from "svelte/store";
+import { get, writable } from "svelte/store";
 import { fetchPhotos } from "$lib/api";
 import { randInt, clamp } from "@utils/helpers";
 import type { Slide } from "@types";
@@ -7,7 +7,8 @@ import type { Slide } from "@types";
  * Global slides store (singleton).
  */
 function createSlidesStore() {
-	const { subscribe, set } = writable<Slide[]>([]);
+	const store = writable<Slide[]>([]);
+	const { subscribe, set } = store;
 
 	async function fetchSlides(limit: number) {
 		return await fetchPhotos({
@@ -18,21 +19,21 @@ function createSlidesStore() {
 
 	async function init(limit: number) {
 		const slides = await fetchSlides(limit);
+		console.log({ slides });
 		set(slides);
+	}
+
+	function getRandomSlides(length: number) {
+		return get(store)
+			.sort(() => 0.5 - Math.random())
+			.slice(0, length);
 	}
 
 	return {
 		subscribe,
-		init
+		init,
+		getRandomSlides
 	};
 }
 
 export const slides = createSlidesStore();
-
-export const randomSlides = (length: number) => {
-	return derived(slides, $slides => {
-		return $slides
-		.sort(() => 0.5 - Math.random())
-		.slice(0, length);
-	})
-};
