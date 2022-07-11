91e297ee8aa79bd885cb7e3878af83e773fb4d7d
Eric Rovell <ericrovell@gmail.com>
2022-07-11 15:58:29 +0300

    feat(slidy-docs): `<Footer />` layout component

---
+++ www/src/lib/components/footer/Footer.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { pathAuthor, pathGithub, pathLicence } from "$lib/paths";
+  import { Link } from "../link";
+
+  import styles from "./footer.module.css";
+</script>
+
+<footer class="{styles.footer} surface-2">
+  <div class="container wrapper">
+    <section label="user">
+      <span>
+        <Link href="{pathGithub}">Slidy</Link> is released under the <Link href="{pathLicence}">MIT licence</Link>
+      </span>
+      <span>
+        © {new Date().getFullYear()} <Link href="{pathAuthor}">Alexander Volkov</Link>
+      </span>
+    </section>
+  </div>
+</footer>
