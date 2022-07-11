08879e9e388bdc9fc5a6b5239af797b81629167f
Eric Rovell <ericrovell@gmail.com>
2022-07-11 15:59:00 +0300

    feat(slidy-docs): paths dir

---
+++ www/src/lib/paths.ts
@@ -0,0 +1,8 @@
+export const pathAuthor = "https://github.com/Valexr";
+export const pathGithub = "https://github.com/Valexr/Slidy";
+export const pathLicence = "https://github.com/Valexr/Slidy/blob/master/LICENSE";
+export const pathPackageAnimation = "https://github.com/Valexr/slidy/tree/master/packages/animation"
+export const pathPackageCore = "https://github.com/Valexr/slidy/tree/master/packages/core";
+export const pathPackageEasing = "https://github.com/Valexr/slidy/tree/master/packages/media";
+export const pathPackageMedia = "https://github.com/Valexr/slidy/tree/master/packages/media";
+export const pathPackageSvelte = "https://github.com/Valexr/slidy/tree/master/packages/svelte";
