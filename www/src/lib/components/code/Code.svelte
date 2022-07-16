bf094613ae14e5dd36f7af13a94cbe89db7851b0
Eric Rovell <ericrovell@gmail.com>
2022-07-16 13:26:33 +0300

    feat(slidy-docs): `Code` component

---
+++ www/src/lib/components/code/Code.svelte
@@ -0,0 +1,18 @@
+<script>
+	import "./code.styles.css";
+	import "./code-theme.styles.css";
+</script>
+
+<!--
+	@component
+	
+	Custom `code` html element to use in blogposts.
+	Provides a syntax highlight.
+
+	! Highlight styles are global, should be careful.
+-->
+<pre>
+	<code>
+		<slot />
+	</code>
+</pre>
