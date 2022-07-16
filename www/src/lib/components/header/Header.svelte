f8367905eb795bbc7275d473d5b45bdbea22208c
Eric Rovell <ericrovell@gmail.com>
2022-07-16 13:26:53 +0300

    feat(slidy-docs): `Hero` header component

---
+++ www/src/lib/components/header/Header.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import styles from "./header.module.css";
+
+	export let className = "";
+	export let title: string | undefined = undefined;
+</script>
+
+<header class="{styles.hero} {className}">
+	{#if title}
+		<h1 class="{styles.title}">
+			{title}
+		</h1>
+	{/if}
+	<slot />
+</header>
