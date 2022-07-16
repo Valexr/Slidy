a4d3451b563699645a47ad03539fcbb587925954
Eric Rovell <ericrovell@gmail.com>
2022-07-16 13:28:09 +0300

    chore(slidy-docs): update components index

--- www/src/lib/components/index.ts
+++ www/src/lib/components/index.ts
@@ -1,5 +1,8 @@
+export { Code } from "./code";
+export { Header } from "./header";
 export { Footer } from "./footer";
 export { Icon } from "./svg";
 export { Link } from "./link";
 export { Masthead } from "./masthead";
+import { Page } from "./page";
 export * as Menu from "./menu";
