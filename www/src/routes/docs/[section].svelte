fb937d0f280d67d3e201caa358f0035aff1caeee
Eric Rovell <ericrovell@gmail.com>
2022-07-16 13:29:32 +0300

    feat(slidy-docs): docs dynamic page

---
+++ www/src/routes/docs/[section].svelte
@@ -0,0 +1,41 @@
+<script context="module" lang="ts">
+	import type { Load } from "@sveltejs/kit";
+
+	export const load: Load = async ({ params }) => {
+		const { section } = params;
+		const pages = [];
+
+		try {
+			let contents = import.meta.glob(`/src/content/**/*.svx`);
+
+			for await (const [ filename, module ] of Object.entries(contents)) {
+				if (filename.includes(`/${section}`)) {
+					const page = await module();
+					pages.push(page.default);
+				}
+			}
+
+			return {
+				props: {
+					pages
+				}
+			};
+
+		} catch (error) {
+			return {
+				status: 404,
+				error: "Page Not Found"
+			};
+		}
+	};
+</script>
+
+<script>
+	export let pages;
+</script>
+
+{#key pages}
+	{#each pages as Page}
+		<Page />
+	{/each}
+{/key}
---
+++ www/src/routes/docs/__layout.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+	import { page } from "$app/stores";
+	import { Header, Link } from "@components";
+	import styles from "./.docs.module.css";
+	import stylesArticle from "./.page.module.css";
+
+	const sections = [
+		{
+			section: "Core Script",
+			items: [
+				{ title: "Core", href: "/docs/core" }
+			]
+		},
+		{
+			section: "Integrations",
+			items: [
+				{ title: "Svelte", href: "/docs/svelte" }
+			]
+		},
+		{
+			section: "Extensions",
+			items: [
+				{ title: "Animation", href: "/docs/animation" },
+				{ title: "Easing", href: "/docs/easing" },
+				{ title: "Media", href: "/docs/media" }
+			]
+		}
+	];
+</script>
+
+<main class="{styles.main}">
+	<Header
+		className="surface-2"
+		title="{$page.params.section || "Documentation"}"
+	/>
+	<nav class="surface-2 {styles.nav}">
+		{#each sections as { section, items }}
+			<section>
+				<h2>{section}</h2>
+				<ul>
+					{#each items as { title, href }}
+						<li>
+							<Link {href}>
+								{title}
+							</Link>
+						</li>
+					{/each}
+				</ul>
+			</section>
+		{/each}
+	</nav>
+	<article class="{stylesArticle.article}">
+		<slot />
+	</article>
+</main>
