be4ab365e011df91a4ed9fb4262e93b717b664a1
Eric Rovell <ericrovell@gmail.com>
2022-08-05 14:13:01 +0300

    feat(slidy-docs): create `toc` plugin

--- www/mdsvex.config.js
+++ www/mdsvex.config.js
@@ -1,8 +1,40 @@
-import slug from 'rehype-slug';
+import slug from "rehype-slug";
+
+/**
+* Custom remark plugin for injecting information about `headings` into frontmatter.
+*/
+export const transformerTOC = () => {
+	let visit;
+	let tree_to_string;
+	return async function transformer(tree, vFile) {
+		if (!visit) {
+			tree_to_string = (await import("mdast-util-to-string")).toString;
+			visit = (await import("unist-util-visit")).visit;
+		}
+
+		vFile.data.toc = [];
+		
+		visit(tree, "heading", (node) => {
+			const title = tree_to_string(node);
+			vFile.data.toc.push({
+				level: node.depth,
+				title,
+				id: title.toLowerCase().replace(/\s/g, "-")
+			});
+		});
+
+		if (!vFile.data.fm) {
+			vFile.data.fm = {};
+		}
+
+		vFile.data.fm.toc = vFile.data.toc;
+	};
+};
 
 export const mdsvexConfig = {
-	rehypePlugins: [slug],
+	rehypePlugins: [ slug ],
+	remarkPlugins: [ transformerTOC ],
 	layout: {
-		_: './src/lib/components/page/Page.svelte'
+		_: "./src/lib/components/page/Page.svelte"
 	}
 };
