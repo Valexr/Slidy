94538b5cc06f255f4bfd722d8cbf1d5284207f34
Eric Rovell <ericrovell@gmail.com>
2022-08-05 14:17:46 +0300

    feat(slidy-docs): update docs section, include TOC

--- www/src/routes/docs/[section]/index.svelte
+++ www/src/routes/docs/[section]/index.svelte
@@ -1,9 +1,19 @@
 <script context="module" lang="ts">
 	import type { Load } from "@sveltejs/kit";
 
+	interface TOCItem {
+		level: number;
+		title: string;
+		id: string;
+	}
+
 	export const load: Load = async ({ params }) => {
 		const { section } = params;
 		const pages = [];
+
+		const toc = [];
+		let title = null;
+		let description = null;
 
 		try {
 			let contents = import.meta.glob("/src/content/**/*.svx");
@@ -11,13 +21,25 @@
 			for await (const [ filename, module ] of Object.entries(contents)) {
 				if (filename.includes(`/${section}`)) {
 					const page = await module();
+					const { metadata } = page;
+
+					// title and description are taken from the 1st page
+					if (metadata.title) {
+						title = metadata.title;
+						description = metadata.description;
+					}
+
+					toc.push(...metadata.toc);
 					pages.push(page.default);
 				}
 			}
 
 			return {
 				props: {
-					pages
+					pages,
+					title,
+					description,
+					toc
 				}
 			};
 
@@ -30,12 +52,38 @@
 	};
 </script>
 
-<script>
+<script lang="ts">
+	import { base } from "$app/paths";
+	import { Header, Link } from "@components";
+	import styles from "./.docs.module.css";
+	import stylesArticle from "./.page.module.css";
+
 	export let pages = [];
+	export let title: string;
+	export let description: string;
+	export let toc: TOCItem[];
 </script>
 
-{#key pages}
-	{#each pages as Page}
-		<Page />
-	{/each}
-{/key}
+<main class="{styles.main}">
+	<Header	className="surface-2"	{title}>
+		<p>{description}</p>
+	</Header>
+	<nav class="surface-1 {styles.nav}">
+		<ol>
+			{#each toc as { level, title, id }}
+				<li style:--toc-level={level} data-level={level}>
+					<Link href="{base}#{id}">
+						{title}
+					</Link>
+				</li>
+			{/each}
+		</ol>
+	</nav>
+	<article class="{stylesArticle.article}">
+		{#key pages}
+			{#each pages as Page}
+				<Page />
+			{/each}
+		{/key}
+	</article>
+</main>
--- www/src/routes/docs/index.svelte
+++ www/src/routes/docs/index.svelte
@@ -1,1 +1,82 @@
-<slot />
+<script lang="ts">
+	import { base } from "$app/paths";
+	import { page } from "$app/stores";
+	import { Header, Link } from "@components";
+
+	const sections = [
+		{
+			section: "Core Script",
+			items: [
+				{ title: "Core", href: "/docs/core" }
+			]
+		},
+		{
+			section: "Integrations",
+			items: [
+				{ title: "Svelte", href: "/docs/svelte" }
+			]
+		},
+		{
+			section: "Extensions",
+			items: [
+				{ title: "Animation", href: "/docs/animation" },
+				{ title: "Easing", href: "/docs/easing" },
+				{ title: "Media", href: "/docs/media" }
+			]
+		}
+	];
+</script>
+
+<main>
+	<Header
+		className="surface-2"
+		title="{$page.params.section || "Documentation"}"
+	/>
+	<div class="contents">
+		{#each sections as { section, items }}
+			<section>
+				<h2>{section}</h2>
+				<ul>
+					{#each items as { title, href }}
+						<li>
+							<Link href="{base}{href}">
+								{title}
+							</Link>
+						</li>
+					{/each}
+				</ul>
+			</section>
+		{/each}
+	</div>
+</main>
+
+<style>
+	main {
+		display: grid;
+		width: 100%;
+		justify-items: center;
+	}
+
+	.contents {
+		display: flex;
+		flex-direction: column;
+		gap: var(--size-3);
+		margin-top: var(--size-5);
+	}
+
+	h2 {
+		margin-bottom: var(--size-2);
+	}
+
+	ul {
+		display: flex;
+		gap: var(--size-3);
+	}
+
+	li {
+		width: fit-content;
+		border: 1px solid white;
+		padding: var(--size-2);
+		border-radius: var(--radius-2);
+	}
+</style>
