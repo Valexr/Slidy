222c321104ee74dd074e1f3faa7dfbeec849fc18
Eric Rovell <ericrovell@gmail.com>
2022-08-13 17:54:41 +0300

    chore(slidy-docs): set general information at home

--- www/src/routes/index.svelte
+++ www/src/routes/index.svelte
@@ -1,36 +1,23 @@
-<!-- <script lang="ts" context="module">
-	import { media, type Queries } from '$lib/media';
-	
-	export async function load({ session }) {
-		let { theme = JSON.stringify(media.matches) } = session.user;
-
-		return {
-			props: {
-				theme: JSON.parse(theme)
-			}
-		};
-	}
-</script> -->
-
 <script lang="ts">
-	import { Slidy } from "@slidy/svelte";
-	import { stairs } from "@slidy/animation";
-	import { Header, Icon, Link } from "@components";
-	import {
-		pathPackageAnimation,
-		pathPackageCore,
-		pathPackageEasing,
-		pathPackageMedia,
-		pathPackageSvelte
-	} from "$lib/paths";
-	//export let theme: Queries = media.matches;
+	import { Header, Icon } from "@components";
 
-	const packages = [
-		{ id: 0, name: "@slidy/animation", href: pathPackageAnimation },
-		{ id: 1, name: "@slidy/core", href: pathPackageCore },
-		{ id: 2, name: "@slidy/easing", href: pathPackageEasing },
-		{ id: 3, name: "@slidy/media", href: pathPackageMedia },
-		{ id: 4, name: "@slidy/svelte", href: pathPackageSvelte }
+	const features = [
+		{
+			title: "Simple and Configurable.",
+			description: "Easy to use.",
+		},
+		{
+			title: "Rich Ecosystem",
+			description: "Templates, animations, easings, and plugins available.",
+		},
+		{
+			title: "TypeScript",
+			description: "Build with Typescript. Ships with types included.",
+		},
+		{
+			title: "Dependency-Free",
+			description: "No dependencies used.",
+		}
 	];
 </script>
 
@@ -50,24 +37,17 @@
 			Simple, configurable, nested & reusable sliding action script with rich ecosystem: templates, animations, easings & plugins.
 		</p>
 	</Header>
-	<section>
-		<h2>Packages</h2>
-		<Slidy
-			animation={stairs}
-			slides={packages}
-			counter={false}
-			duration={450}
-			gravity={1.45}
-			snap="center"
-			arrows
-			let:item
-		>
-			<article class="surface-3">
-				<Link href={item.href}>
-					{item.name}
-				</Link>
-			</article>
-		</Slidy>
+	<section class="contents">
+		<ul class="features grid-flexible">
+			{#each features as { title, description }}
+				<li>
+					<article class="surface-2 feature">
+						<h2>{title}</h2>
+						<p>{description}</p>
+					</article>
+				</li>
+			{/each}
+		</ul>
 	</section>
 </main>
 
@@ -77,7 +57,7 @@
 	}
 
 	h2 {
-		margin-bottom: var(--size-3);
+		margin-bottom: var(--size-1);
 	}
 
 	main section {
@@ -89,10 +69,17 @@
 		text-align: center;
 	}
 
-	article {
+	.features {
+		gap: var(--size-4);
+		width: 100%;
+		margin: var(--size-8) auto;
+	}
+
+	.feature {
 		display: flex;
+		flex-flow: column nowrap;
 		justify-content: center;
-		align-items: center;
+		align-items: flex-start;
 		padding: var(--size-3);
 		border-radius: var(--size-3);
 	}
