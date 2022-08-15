720c2fc5d09d7e3654e846d107b1d40146960f2e
Eric Rovell <ericrovell@gmail.com>
2022-08-15 03:07:33 +0300

    feat(slidy-svelte): refactor event names & handle page visibility

--- packages/svelte/src/actions/autoplay.ts
+++ packages/svelte/src/actions/autoplay.ts
@@ -13,7 +13,10 @@ const defaults: Options = {
 	interval: 1000
 };
 
-export function autoplayable(node: HTMLElement, parameters: Options = defaults): ReturnType<Action> {
+/**
+ * Sets the interval timer and fires `play`, `pause`, and `stop` events.
+ */
+export function autoplay(node: HTMLElement, parameters: Options = defaults): ReturnType<Action> {
 	let { status = false } = parameters;
 	const { interval = 1000 } = parameters;
 
@@ -22,21 +25,30 @@ export function autoplayable(node: HTMLElement, parameters: Options = defaults):
 	const play = () => {
 		timerInterval = window.setInterval(() => {
 			if (status) {
-				node.dispatchEvent(new CustomEvent("autoplay"));
+				node.dispatchEvent(new CustomEvent("play"));
 			}
 		}, interval);
 	};
 
 	const pause = () => {
 		status = false;
+		node.dispatchEvent(new CustomEvent("pause"));
 	};
 
 	const handlePointerover = () => pause();
 	const handlePointerout = () => status = true;
 	const handleFocusin = () => pause();
 	const handleFocusout = () => status = true;
+	const handleVisibilityChange = () => {
+		if (document.visibilityState === "hidden") {
+			pause();
+		} else {
+			status = true;
+		}
+	};
 
 	const start = () => {
+		window.addEventListener("visibilitychange", handleVisibilityChange, false);
 		node.addEventListener("pointerover", handlePointerover, { passive: true });
 		node.addEventListener("pointerout", handlePointerout, { passive: true });
 		node.addEventListener("focusin", handleFocusin, { passive: true });
@@ -46,11 +58,12 @@ export function autoplayable(node: HTMLElement, parameters: Options = defaults):
 
 	const stop = () => {
 		window.clearInterval(timerInterval);
+		window.removeEventListener("visibilitychange", handleVisibilityChange);
 		node.removeEventListener("pointerover", handlePointerover);
 		node.removeEventListener("pointerout", handlePointerout);
 		node.removeEventListener("focusin", handleFocusin);
 		node.removeEventListener("focusout", handleFocusout);
-		node.dispatchEvent(new CustomEvent("autoplay-stop"));
+		node.dispatchEvent(new CustomEvent("stop"));
 	};
 
 	if (status) {
