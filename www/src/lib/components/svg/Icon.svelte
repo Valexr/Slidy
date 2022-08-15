b0db777f36cf2badf2679ccdd6bb41d7fddd3def
Valexr <valexr@gmail.com>
2022-08-15 09:42:28 +0300

    updeps & simplify logo reuse

--- www/src/lib/components/svg/Icon.svelte
+++ www/src/lib/components/svg/Icon.svelte
@@ -1,25 +1,26 @@
-<script lang="ts">
-	import styles from "./icon.module.css";
-
-	export let className = "";
-	export let path: string | undefined = undefined;
-  export let title: string | undefined = undefined;
-  export let viewBox = "0 0 100 100";  
-</script>
-
 <svelte:options namespace="svg" />
+
+<script lang="ts">
+	import styles from './icon.module.css';
+
+	export let className = '';
+	export let path: string | undefined = undefined;
+	export let title: string | undefined = undefined;
+	export let viewBox = '0 0 100 100';
+</script>
 
 <svg
 	class="{styles.icon} {className}"
-  xmlns="http://www.w3.org/2000/svg"
-  {viewBox}
-  aria-labelledby={title}
-  role="presentation">
-    {#if title}
-      <title>{title}</title>
-    {/if}
-    {#if path}
-      <path d={path} />
-    {/if}
-    <slot />
+	xmlns="http://www.w3.org/2000/svg"
+	{viewBox}
+	aria-labelledby={title}
+	role="presentation"
+>
+	{#if title}
+		<title>{title}</title>
+	{/if}
+	{#if path}
+		<path d={path} />
+	{/if}
+	<slot />
 </svg>
