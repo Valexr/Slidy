b85e628e99468e053407e2386738ccc891e2a96b
Valexr <valexr@gmail.com>
2022-08-25 21:03:06 +0300

    @slid/assets - add helper functions

--- assets/scripts/utils.ts
+++ assets/scripts/utils.ts
@@ -20,3 +20,12 @@ export const format = (pattern: string, ...replacements: (string | number)[]) =>
 export const execute = (...quenue: (undefined | ((arg: any) => void))[]) => {
     return (arg: any) => quenue.forEach((fn) => fn?.(arg));
 };
+
+export const noop = (...args: any[]) => {
+    // this function serves no purpose
+};
+
+export const not = (value: unknown) => !value;
+
+export const increment = (value: number) => value + 1;
+
