88e0846c532faf6f1f5cbf00f86acd9bee99ef91
Valexr <valexr@gmail.com>
2022-09-01 11:41:56 +0000

    Deploying to gh-pages from @ Valexr/Slidy@b48e1da8647e0613853f98cd5520442439b6db02 🚀

---
+++ _app/immutable/chunks/0-f05182d9.js
@@ -0,0 +1,1 @@
+import{default as n}from"../components/pages/_layout.svelte-6d57b7db.js";import"./index-b57ea404.js";import"./masthead.module-a817179b.js";import"./paths-f25f3c46.js";import"./Link-2d43e7e1.js";import"./paths-83def39a.js";import"./stores-8e4c45c3.js";import"./singletons-4406cb3e.js";export{n as component};
--- _app/immutable/chunks/3-e9356f09.js
+++
@@ -1,1 +0,0 @@
-import{default as n}from"../components/pages/docs/_page.svelte-dd265c2a.js";import"./index-b57ea404.js";import"./paths-83def39a.js";import"./stores-aa142dff.js";import"./singletons-cdd2ca76.js";import"./masthead.module-a817179b.js";import"./Header-ade1377d.js";import"./Link-2d43e7e1.js";export{n as component};
