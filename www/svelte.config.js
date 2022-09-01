601b9d4d7061c25ee2452e6dce28a0b6a9ee58c4
Valexr <valexr@gmail.com>
2022-09-01 14:45:22 +0300

    @slidy/www - fix fallback file

--- www/svelte.config.js
+++ www/svelte.config.js
@@ -7,20 +7,20 @@ const dev = process.env.npm_lifecycle_event === 'dev';
 
 /** @type {import("@sveltejs/kit").Config} */
 const config = {
-	preprocess: [mdsvex(mdsvexConfig), preprocess()],
-	extensions: ['.svelte', '.svx'],
-	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: '200.html',
-			precompress: false
-		}),
-		trailingSlash: 'always',
-		paths: {
-			base: dev ? '' : '/Slidy'
-		}
-	}
+    preprocess: [mdsvex(mdsvexConfig), preprocess()],
+    extensions: ['.svelte', '.svx'],
+    kit: {
+        adapter: adapter({
+            pages: 'build',
+            assets: 'build',
+            fallback: 'index.html',
+            precompress: false
+        }),
+        trailingSlash: 'always',
+        paths: {
+            base: dev ? '' : '/Slidy'
+        }
+    }
 };
 
 export default config;
