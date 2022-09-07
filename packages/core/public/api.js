107ef084743b63c7f3671501651865fb742fb854
Valexr <valexr@gmail.com>
2022-09-07 10:59:58 +0300

    @slidy/core - fake img api

--- packages/core/public/api.js
+++ packages/core/public/api.js
@@ -1,52 +1,46 @@
 export async function getPhotos(node, page, limit) {
     node.innerHTML = `Loading... 🚀`;
     //page: 38 - END,  61 - START, 28
-    try {
-        const res = await fetch(`https://picsum.photos/v2/list?limit=${limit}&page=${page}`, {
-            mode: 'cors'
-        });
-        const photos = await res.json();
-        if (photos.length === limit && node.isConnected) {
-            node.innerHTML = createSlides(node, photos);
-            thumbs.innerHTML = createSlides(thumbs, photos);
-            dots.innerHTML = createSlides(dots, photos);
 
-            if (node.children.length === limit) {
-                const mounted = Array.from(node.children).every(
-                    (child) => child && child.isConnected
-                );
-            }
+    try {
+        const indexes = [...Array(limit).keys()];
+        const width = node.clientWidth * devicePixelRatio;
+        const height = node.clientHeight * devicePixelRatio;
+        const sizes = indexes.map(() => [utils.randomQ(width, height), utils.randomQ(height, width)]);
+
+        if (node.isConnected && sizes.length) {
+            node.innerHTML = createSlides(node, sizes);
+            thumbs.innerHTML = createSlides(thumbs, sizes);
+            dots.innerHTML = createSlides(dots, sizes);
         } else {
             node.innerHTML = `Slidy haven't items 🤷🏻‍♂️`;
         }
     } catch (error) {
         console.error(error);
+        node.innerHTML = error;
     }
 
-    function createSlides(node, photos) {
-        const nodes = photos.map((p, i) => {
+    function createSlides(node, sizes) {
+        const slides = sizes.map(([width, height], i) => {
             const aspect = (value, ratio = false) => {
                 const pr = ratio ? devicePixelRatio : 1;
-                const asp =
-                    aspectQ(p.width, p.height, node.clientWidth, node.clientHeight)[value] * pr;
+                const nodeW = node.clientWidth;
+                const nodeH = node.clientHeight;
+                const asp = aspectQ(width, height, nodeW, nodeH)[value] * pr;
                 return Math.round(asp);
             };
-            const src = `https://picsum.photos/id/${p.id}/${aspect('width', true)}/${aspect(
-                'height',
-                true
-            )}.jpg`;
-            const background = `background-image: url(https://picsum.photos/id/${p.id}/${100 * devicePixelRatio
-                }/${100 * devicePixelRatio}.jpg)`;
+            const src = `https://source.unsplash.com/random/${aspect('width', true)}x${aspect('height', true)}`;
+            const imgSize = `width="${aspect('width')}" height="${aspect('height')}"`;
+            const background = `background-image: url(${src})`;
+            const alt = `unsplashPhoto#${i}`;
 
             if (node.id === 'node') {
-                return `<li id="${i}"><img src="${src}" width="${aspect('width')}" height="${aspect(
-                    'height'
-                )}" alt="${p.author}"/></li>`;
+                return `<li id="${i}"><img src="${src}" ${imgSize} alt="${alt}"/></li>`;
             } else if (node.id === 'thumbs') {
                 return `<button id="${i}" style="${background}" width="100" height="100">${i}</button>`;
             } else return `<button id="${i}">${i}</button>`;
         });
-        return nodes.join('');
+        return slides.join('');
     }
 
     function aspectQ(srcWidth, srcHeight, maxWidth, maxHeight) {
