fc355344065f7dd908bf229507ba64dd3951a1ba
Valexr <valexr@gmail.com>
2022-09-14 19:15:14 +0300

    @slidy/easing - min duration 450

--- packages/easing/src/easing.ts
+++ packages/easing/src/easing.ts
@@ -1,10 +1,21 @@
 import type { Easing } from './types';
 
+const { cos, sin, pow, sqrt, PI } = Math
+
 const linear: Easing = (t) => t;
 const quad: Easing = (t) => t * t;
 const cubic: Easing = (t) => t * t * t;
 const quart: Easing = (t) => t * t * t * t;
 const quint: Easing = (t) => t * t * t * t * t;
+const sine: Easing = (t) => 1 - cos((t * PI) / 2);
+const expo: Easing = (t) => (t === 0 ? 0 : pow(2, 10 * t - 10));
+const circ: Easing = (t) => 1 - sqrt(1 - pow(t, 2));
+
+const back: Easing = (t) => {
+    const c1 = 1.70158;
+    const c3 = c1 + 1;
+    return c3 * t * t * t - c1 * t * t;
+};
 const bounce: Easing = (t) => {
     const out: Easing = (t) => {
         const n1 = 7.5625;
@@ -22,17 +33,10 @@ const bounce: Easing = (t) => {
     };
     return 1 - out(1 - t);
 };
-const sine: Easing = (t) => 1 - Math.cos((t * Math.PI) / 2);
-const expo: Easing = (t) => (t === 0 ? 0 : Math.pow(2, 10 * t - 10));
 const elastic: Easing = (t) => {
-    const c4 = (2 * Math.PI) / 3;
-    return t === 0 ? 0 : t === 1 ? 1 : -Math.pow(2, 10 * t - 10) * Math.sin((t * 10 - 10.75) * c4);
-};
-const circ: Easing = (t) => 1 - Math.sqrt(1 - Math.pow(t, 2));
-const back: Easing = (t) => {
-    const c1 = 1.70158;
-    const c3 = c1 + 1;
-    return c3 * t * t * t - c1 * t * t;
+    const c4 = (2 * PI) / 3;
+    return t % 1 ? -pow(2, 10 * t - 10) * sin((t * 10 - 10.75) * c4) : t;
 };
 
 export { linear, quad, cubic, quart, quint, bounce, sine, expo, elastic, circ, back };
+
