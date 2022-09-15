1a54b78ae98c8a09d0558c406deadb09d5bed362
Artemiy <shuckinart@yandex.ru>
2022-09-15 18:02:16 +0000

    @slidy/solid - better `channel`

--- packages/solid/src/dev/lib/channel.ts
+++ packages/solid/src/dev/lib/channel.ts
@@ -1,25 +1,24 @@
 import { createSignal } from 'solid-js';
+import type { Setter } from 'solid-js'
 
 interface Channel<T> {
     (): T;
-    (setter: T | ((prev: T) => T)): T;
+    (setter: Setter<T>): T;
 }
 
 function channel<T>(init: T): Channel<T> {
     const [get, set] = createSignal(init);
 
-    function controller(): T;
-    function controller(setter: ((prev: T) => T) | T): T;
-    function controller(setter?: ((prev: T) => T) | T): T {
+    function controller(setter?: Setter<T>) {
         if (arguments.length === 0) {
             return get();
         } else {
             // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-            return set((prev) => (setter instanceof Function ? setter(prev) : setter!));
+            return set(setter!);
         }
     }
 
-    return controller;
+    return controller as Channel<T>;
 }
 
 export { channel };
