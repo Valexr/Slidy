45e5f06202e2256b36fc7517b5649be50f84c4f0
Valexr <valexr@gmail.com>
2022-09-17 21:36:32 +0300

    @slidy/plugins - idea cb in play()

--- packages/plugins/src/lib/play.ts
+++ packages/plugins/src/lib/play.ts
@@ -1,14 +1,18 @@
 import { timer as IntervalTimer } from '../utils/env'
 import type { PluginArgs } from '../types'
 
-export function play(params = { duration: 1000, delay: 0 }) {
+export function play(params?: { duration: number, delay: number }, cb?: () => void) {
+
+    if (!params || !Object.keys(params).length) {
+        params = { duration: 1000, delay: 0 }
+    }
+
     const { duration, delay } = params
 
     return ({ node, options, instance }: PluginArgs) => {
+        cb ||= () => instance.to((options.index as number) + 1)
 
-        const timer = IntervalTimer(() => {
-            instance.to((options.index as number) + 1)
-        }, duration, delay);
+        const timer = IntervalTimer(cb, duration, delay);
 
         node.addEventListener('mount', mount)
         node.addEventListener('destroy', timer.stop);
