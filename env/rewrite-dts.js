2c028eb27845fe41e9ad2c36990316a263d9eb14
Artemiy <shuckinart@yandex.ru>
2022-09-17 14:52:15 +0000

    ᅠ

--- env/rewrite-dts.js
+++ env/rewrite-dts.js
@@ -4,26 +4,29 @@ import path from 'path';
 const cwd = process.cwd();
 const re = /declare module "(.*?)"/g;
 
-export default async function main() {
-    const paths = {
-        'dts': path.resolve(cwd, 'dist', 'index.d.ts'),
-        'package.json': path.resolve(cwd, 'package.json')
-    }
+/**
+ * @param {string} file
+ */
+async function readJSON(file) {
+    const resolved = path.resolve(cwd, file);
+    const content = await fs.readFile(resolved, 'utf-8');
 
-    let content = await fs.readFile(paths['dts'], 'utf-8');
+    return JSON.parse(content);
+}
+
+export default async function main() {
+    const dtsFilePath = path.resolve(cwd, 'dist', 'index.d.ts');
+    const content = await fs.readFile(dtsFilePath, 'utf-8');
 
     const declarations = content.match(re);
 
-    if (declarations === null) throw new Error(`No found declarations at ${paths['dts']}`);
+    if (declarations === null) throw new Error(`No found declarations at ${dtsFilePath}`);
 
-    const latest = declarations.at(-1);
+    const json = await readJSON('package.json');
 
-    const packageJson = await fs.readFile(paths['package.json'], 'utf-8');
-    const json = JSON.parse(packageJson);
+    const edited = content.replace(declarations.at(-1).slice(16, -1), String(json.name));
 
-    content = content.replace(latest.slice(16, -1), String(json.name));
-
-    await fs.writeFile(paths['dts'], content, 'utf-8')
+    await fs.writeFile(dtsFilePath, edited, 'utf-8')
 }
 
 main();
