d535dbf839bc74a4602fad150b99f28fb05aa387
Artemiy <shuckinart@yandex.ru>
2022-09-17 09:58:23 +0000

    @slidy/solid - 🤷

--- packages/solid/src/components/Core/Core.tsx
+++ packages/solid/src/components/Core/Core.tsx
@@ -45,7 +45,7 @@ const Core: FlowComponent<Partial<Props>> = ($props) => {
 
     const useSlidy = (el: HTMLElement) => {
         const fn = () => {
-            const { update, destroy } = slidy(el, options);
+            const { update, destroy } = slidy(el, Object.assign({}, options));
 
             createEffect(() => {
                 update(options);
