895f54d4a20cd18638f67f099ffdb3ca5660f3d8
Artemiy <shuckinart@yandex.ru>
2022-09-19 15:08:35 +0000

    @slidy/plugins(fool) - do not call .update()

--- packages/plugins/src/lib/fool.ts
+++ packages/plugins/src/lib/fool.ts
@@ -5,12 +5,14 @@ import type { PluginArgs } from '../types';
  * Plug it on at April Fool's Day and you won't get fired!
  */
 export function fool() {
-    return ({ node, instance }: PluginArgs) => {
+    return ({ node, options }: PluginArgs) => {
         function index(event: CustomEvent<{ index: number }>) {
             const index = event.detail.index;
 
             Promise.resolve(index % 2 === 0).then((val) => {
-                Promise.resolve({ axis: val ? 'x' : 'y' } as const).then(instance.update)
+                Promise.resolve(val ? 'x' : 'y').then((axis) => {
+                    options['axis'] = axis as 'x' | 'y';
+                });
             });
         }
 
