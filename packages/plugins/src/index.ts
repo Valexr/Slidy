a42b61467c2f259d464f95bb3e981cc3b37cbff8
Artemiy <shuckinart@yandex.ru>
2022-09-19 13:50:42 +0000

    @slidy/plugins - add joke plugin

--- packages/plugins/src/index.ts
+++ packages/plugins/src/index.ts
@@ -1,4 +1,5 @@
 export { log } from './lib/log';
 export { play } from './lib/play';
 export { marquee } from './lib/marquee';
+export { fool } from './lib/fool'
 export type { PluginArgs, PluginFunc } from './types';
---
+++ packages/plugins/src/lib/fool.ts
@@ -0,0 +1,24 @@
+import type { PluginArgs } from '../types';
+
+/**
+ * Changes axis depending on the parity of the slide index
+ * Plug it on at April Fool's Day and you won't get fired!
+ */
+export function fool() {
+    return ({ node, instance }: PluginArgs) => {
+        function index(event: CustomEvent<{ index: number }>) {
+            const index = event.detail.index;
+
+            Promise.resolve(index % 2 === 0).then((val) => {
+                instance.update({ axis: val ? 'x' : 'y' });
+            });
+        }
+
+        node.addEventListener('destroy', function destroy() {
+            node.removeEventListener('destroy', destroy);
+            node.removeEventListener('index', index as EventListener);
+        });
+
+        node.addEventListener('index', index as EventListener);
+    };
+}
