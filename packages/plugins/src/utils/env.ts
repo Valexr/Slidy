eebb856165b3277e40a2365a4ee71fbcc77fcd56
Valexr <valexr@gmail.com>
2022-12-13 11:49:55 +0300

    separate timer in plugins utils

--- packages/plugins/src/utils/env.ts
+++ packages/plugins/src/utils/env.ts
@@ -1,47 +1,5 @@
 import { loop } from './utils';
-import type { EventMap, TimerInstace } from '../types';
-
-function timer(callback: () => void, interval: number, delay = 0): TimerInstace {
-    let tid: NodeJS.Timer,
-        state = 0, //  0 = idle, 1 = running, 2 = paused, 3 = resumed
-        start = 0,
-        remaining = interval
-
-    function pause() {
-        if (state !== 1) return;
-        state = 2;
-        clearInterval(tid);
-        remaining = delay || interval - Math.abs(performance.now() - start);
-    }
-
-    function resume() {
-        if (state !== 2) return;
-        state = 3;
-        setTimeout(timeoutCallback, remaining);
-    }
-
-    function stop() {
-        state = 0;
-        clearInterval(tid);
-    }
-
-    function play() {
-        state = 1;
-        start = performance.now();
-        tid = setInterval(() => {
-            callback()
-            start = performance.now();
-        }, interval);
-    }
-
-    function timeoutCallback() {
-        if (state !== 3) return;
-        callback();
-        play()
-    }
-
-    return { play, pause, resume, stop };
-}
+import type { EventMap } from '../types';
 
 function listen(node: HTMLElement, events: EventMap, on = true): void {
     loop(events, (item) => {
@@ -57,4 +15,4 @@ function dispatch(node: HTMLElement, event: string, detail?: EventInit) {
     return node.dispatchEvent(EVENT);
 }
 
-export { timer, type TimerInstace, listen, dispatch };
+export { listen, dispatch };
---
+++ packages/plugins/src/utils/timer.ts
@@ -0,0 +1,45 @@
+import type { TimerInstace } from '../types';
+
+function timer(callback: () => void, interval: number, delay = 0): TimerInstace {
+    let tid: NodeJS.Timer,
+        state = 0, //  0 = idle, 1 = running, 2 = paused, 3 = resumed
+        start = 0,
+        remaining = interval
+
+    function pause() {
+        if (state !== 1) return;
+        state = 2;
+        clearInterval(tid);
+        remaining = delay || interval - Math.abs(performance.now() - start);
+    }
+
+    function resume() {
+        if (state !== 2) return;
+        state = 3;
+        setTimeout(timeoutCallback, remaining);
+    }
+
+    function stop() {
+        state = 0;
+        clearInterval(tid);
+    }
+
+    function play() {
+        state = 1;
+        start = performance.now();
+        tid = setInterval(() => {
+            callback()
+            start = performance.now();
+        }, interval);
+    }
+
+    function timeoutCallback() {
+        if (state !== 3) return;
+        callback();
+        play()
+    }
+
+    return { play, pause, resume, stop };
+}
+
+export { timer, type TimerInstace };
