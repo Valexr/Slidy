02268f3470071c555e165805fa040ca011a03630
Valexr <valexr@gmail.com>
2023-02-02 20:38:30 +0300

    @slidy/plugins - fix TaskQueue

--- packages/plugins/src/lib/autoplay/task-queue.ts
+++ packages/plugins/src/lib/autoplay/task-queue.ts
@@ -8,59 +8,59 @@ type Timeout = /** number */ NodeJS.Timeout;
 const now = performance.now.bind(performance);
 
 class TaskQueue {
-  protected queue: Queue;
-  protected index: number;
-  protected timeoutId: Timeout;
-  protected off: number = 0;
+    protected queue: Queue;
+    protected timeoutId!: Timeout;
+    protected index: number;
+    protected off: number;
 
-  protected time = now();
+    protected time = now();
 
-  constructor(queue: Queue) {
-    this.queue = queue;
-    this.index = 0;
-    this.timeoutId = null as any as Timeout;
-  }
-
-  public start() {
-    this.time = now();
-    this.runNextTask();
-  }
-
-  public pause() {
-    clearTimeout(this.timeoutId);
-    this.off += now() - this.time;
-  }
-
-  public stop() {
-    clearTimeout(this.timeoutId);
-
-    this.off = 0;
-    this.index = 0;
-  }
-
-  protected runNextTask() {
-    if (this.index === this.queue.length) {
-      this.stop();
+    constructor(queue: Queue) {
+        this.queue = queue;
+        this.index = 0;
+        this.off = 0
     }
 
-    const task = this.queue[this.index];
-
-    if (typeof task === 'function') {
-      const start = now();
-      task();
-
-      this.index++;
-      this.off += now() - start;
-      this.runNextTask();
-    } else {
-      this.timeoutId = setTimeout(() => {
-        this.index++;
+    public start() {
         this.time = now();
-        this.off = 0;
         this.runNextTask();
-      }, task.await - this.off)
     }
-  }
+
+    public pause() {
+        clearTimeout(this.timeoutId);
+        this.off += now() - this.time;
+    }
+
+    public stop() {
+        clearTimeout(this.timeoutId);
+
+        this.off = 0;
+        this.index = 0;
+    }
+
+    protected runNextTask() {
+        if (this.index === this.queue.length) {
+            this.stop();
+        }
+
+        const task = this.queue[this.index];
+
+        if (typeof task === 'function') {
+            const start = now();
+            task();
+
+            this.index++;
+            this.off += now() - start;
+            this.runNextTask();
+        } else {
+            this.timeoutId = setTimeout(() => {
+                this.index++;
+                this.time = now();
+                this.off = 0;
+                this.runNextTask();
+            }, task.await - this.off)
+        }
+    }
 }
 
 export { TaskQueue }
