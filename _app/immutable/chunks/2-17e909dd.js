60bba4fc4b38e1a9bb838661a173bbe23c81fff1
Valexr <valexr@gmail.com>
2023-02-03 14:39:53 +0000

    Deploying to gh-pages from @ Valexr/Slidy@b9de23ac9b2d7de016b88c7c5ab2a6a7fbf3b354 🚀

--- _app/immutable/chunks/2-17e909dd.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-6e622de0.js";export{t as component};
---
+++ _app/immutable/chunks/2-ff5c0a29.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-ce99ebfa.js";export{t as component};
