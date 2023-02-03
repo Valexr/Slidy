2627ae48969fdb207d7f9affdb2f85a74b3e41c8
Valexr <valexr@gmail.com>
2023-02-03 12:51:48 +0300

    www - cleanup

--- www/src/routes/+layout.svelte
+++ www/src/routes/+layout.svelte
@@ -1,16 +1,10 @@
 <script lang="ts" context="module">
-	import { page } from '$app/stores';
 	import { Footer, Masthead } from '@components';
 
 	import '../styles/globals.css';
 	import '../styles/tokens.css';
 	import '../styles/utility.css';
 	import '../styles/typography.css';
-</script>
-
-<script lang="ts">
-	import { media } from '$lib/media';
-	console.log($media, $page.data.media);
 </script>
 
 <Masthead />
