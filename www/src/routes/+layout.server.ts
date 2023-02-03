5147a5c5e2d7298fc9fb8e2edb21525c18a5b5dd
Valexr <valexr@gmail.com>
2023-02-03 12:34:29 +0300

    www - fix updeps

--- www/src/routes/+layout.server.ts
+++ www/src/routes/+layout.server.ts
@@ -1,2 +1,9 @@
+import type { LayoutServerLoad } from './$types';
+
+export const load = (({ cookies }) => {
+    const media = JSON.parse(cookies.get('media') || '{}');
+    return { media };
+}) satisfies LayoutServerLoad;
+
 export const prerender = true;
 export const trailingSlash = 'always';
--- www/src/routes/+layout.svelte
+++ www/src/routes/+layout.svelte
@@ -1,10 +1,16 @@
-<script>
+<script lang="ts" context="module">
+	import { page } from '$app/stores';
 	import { Footer, Masthead } from '@components';
 
 	import '../styles/globals.css';
 	import '../styles/tokens.css';
 	import '../styles/utility.css';
 	import '../styles/typography.css';
+</script>
+
+<script lang="ts">
+	import { media } from '$lib/media';
+	console.log($media, $page.data.media);
 </script>
 
 <Masthead />
