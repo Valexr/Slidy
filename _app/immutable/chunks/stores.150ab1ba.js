a52b80f69130320df63786b6c34b3f8cf8ef66bf
Valexr <valexr@gmail.com>
2023-05-10 08:01:17 +0000

    Deploying to gh-pages from @ Valexr/Slidy@0bff24126ca05fa687bab239f9f5cbe1edf59365 🚀

--- _app/immutable/chunks/stores.150ab1ba.js
+++
@@ -1,1 +0,0 @@
-import"./index.825197f3.js";import{s as e}from"./singletons.1ea88d88.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
