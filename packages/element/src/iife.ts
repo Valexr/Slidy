7afa23e8b42032996649143ae029f9997f3173f5
Artemiy Schukin <shuckinart@yandex.ru>
2024-01-23 22:16:58 +0300

    fix core build

--- packages/element/src/iife.ts
+++ packages/element/src/iife.ts
@@ -1,6 +1,6 @@
 export { default as element } from './lib/slidy';
-export * as animation from '@slidy/animation/src';
-export * as easing from '@slidy/easing/src';
+export * as animation from '@slidy/animation';
+export * as easing from '@slidy/easing';
 export * as plugin from '@slidy/plugins/src';
 export { media } from '@slidy/media/src';
 export type { Options, EasingFunc, AnimationFunc, AnimationArgs } from '@slidy/core';
